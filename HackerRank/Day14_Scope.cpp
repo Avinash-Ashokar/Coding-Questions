9a8f271799c3939bbf1eee7fbdd9d46164b30730
Avinash-Ashokar <mail_avinashashokar@pm.me>
2021-09-10 14:04:27 +0530

    Day14 Solution

---
+++ HackerRank/Day14_Scope.cpp
@@ -0,0 +1,51 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+class Difference {
+    private:
+    vector<int> elements;
+  
+  	public:
+  	int maximumDifference;
+      
+    Difference(vector<int> e) {
+        this->maximumDifference =0;
+        this->elements = e;
+    }
+    void computeDifference() {
+        for (int i=0; i<elements.size();i++) {
+            for (int j=i; j<elements.size();j++) {
+                if (abs(elements[i]-elements[j]) > maximumDifference) {
+                    maximumDifference = abs(elements[i] - elements[j]);
+                }
+            }
+        }
+    }
+}; // End of Difference class
+
+int main() {
+    int N;
+    cin >> N;
+    
+    vector<int> a;
+    
+    for (int i = 0; i < N; i++) {
+        int e;
+        cin >> e;
+        
+        a.push_back(e);
+    }
+    
+    Difference d(a);
+    
+    d.computeDifference();
+    
+    cout << d.maximumDifference;
+    
+    return 0;
+}
